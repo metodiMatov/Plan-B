88f2fdd6c19179066c83082dd87372b3ac262f8a
metodiMatov <metodi.matov@gmail.com>
2017-10-08 21:43:29 +0300

    sync

--- assets/js/userFlight.js
+++ assets/js/userFlight.js
@@ -1,29 +1,31 @@
 var ticket = (function () {
     var number = 1;
-    function Ticket(origin, destination, clasa, baggage,isOneWay,classPrice,baggagePrice) {
+    function Ticket(origin, destination, date, hour, isOneWay, classPrice, baggagePrice) {
         this.id = number++;
         this.isOneWay = isOneWay;
         this.destination = destination;
         this.origin = origin;
-        this.class = clasa;
-        this.baggage = baggage;
-        this.totalPrice = 0;
-        this.isOneWay = isOneWay;
+        this.date = date;
+        this.hour = hour;
+        this.baggagePrice = baggagePrice;
+        this.classPrice = classPrice;
+        this.price = 0;
     }
     Ticket.prototype.claculatedPrice = function () {
         if (this.isOneWay) {
-            this.totalPrice = this.classPrice + this.baggagePrice;
+            this.price = this.classPrice + this.baggagePrice;
         } else {
-            this.totalPrice = 2 * (this.classPrice + this.baggagePrice);
+            this.price = 2 * (this.classPrice + this.baggagePrice);
         }
     }
-    Ticket.prototype.addProperties = function (origin, destination, clasa, baggage, calculatedPrice,isOneWay) {
+    Ticket.prototype.addProperties = function (origin, destination, date, hour, isOneWay, classPrice, baggagePrice) {
         this.isOneWay = isOneWay;
         this.destination = destination;
         this.origin = origin;
-        this.class = clasa;
-        this.baggage = baggage;
-        this.isOneWay = isOneWay;
+        this.date = date;
+        this.hour = hour;
+        this.baggagePrice = baggagePrice;
+        this.classPrice = classPrice;
     }
     return new Ticket();
 })();
